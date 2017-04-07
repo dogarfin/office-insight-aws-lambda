150b0d8da82750911670153980e5412256e6c206
sowiecki <nase00@gmail.com>
2017-04-07 14:07:01 -0500

    Conform to new GTFO pings api

--- src/config.js
+++ src/config.js
@@ -11,6 +11,7 @@ export const GTFO_HOST = 'acheron.herokuapp.com';
 export const GTFO_PORT = '80';
 export const GTFO_PING = '/api/ping';
 export const GTFO_MAP_ANCHOR = 'west-lobby';
+export const GTFO_ID = 'office-insights-cxm';
 
 export const PRODUCTION_HOST_AVAILABILITY = 'http://ec2-50-16-10-211.compute-1.amazonaws.com';
 export const PRODUCTION_HOST_PORT = '8080';
